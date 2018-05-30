9b5b061e89bce5e1ccdddb71b12d9e3aea80fa7d
ronniecm <ronniecm@gmail.com>
2018-05-30 19:32:30 -0400

    Finished Four Function Calculator

--- src/GUI/FourFunctionCalculator.java
+++ src/GUI/FourFunctionCalculator.java
@@ -9,6 +9,7 @@ public class FourFunctionCalculator extends JPanel {
 	private JPanel opButtons;
 	private NumberButton[][] numButtons;
 	private JTextField calcText;
+	private JButton decimal;
 	private OpButton add;
 	private OpButton subtract;
 	private OpButton multiply;
@@ -16,11 +17,14 @@ public class FourFunctionCalculator extends JPanel {
 	private NumberButton zero;
 	private JButton equals;
 	private String op;
-	private int[] nums;
+	private double[] nums;
 	private JButton clear;
+	private int pushes = 0;
+	private boolean pushed = false;
+	
 	public FourFunctionCalculator()
 	{
-		nums = new int[2];
+		nums = new double[2];
 		setLayout(new BorderLayout());
 		
 		buttons = new JPanel();
@@ -43,12 +47,16 @@ public class FourFunctionCalculator extends JPanel {
 		
 		
 		calcText = new JTextField("0", 10);
+		calcText.setFont(new Font("Times New Roman", Font.PLAIN, 36));
+		calcText.setHorizontalAlignment(SwingConstants.RIGHT);
 		add(calcText, BorderLayout.NORTH);
 		
 		opButtons = new JPanel();
 		opButtons.setLayout(new FlowLayout());
 		add(opButtons, BorderLayout.SOUTH);
 		
+		decimal = new JButton(".");
+		decimal.addActionListener(new NumListener(101, 101));
 		zero = new NumberButton(0);
 		zero.addActionListener(new NumListener(100, 100));
 		add = new OpButton("+");
@@ -63,12 +71,14 @@ public class FourFunctionCalculator extends JPanel {
 		clear.addActionListener(new Clear());
 		
 		
+		opButtons.add(decimal);
 		opButtons.add(add);
 		opButtons.add(subtract);
 		opButtons.add(zero);
 		opButtons.add(multiply);
 		opButtons.add(divide);
 		opButtons.add(clear);
+
 		
 		equals = new JButton("=");
 		equals.addActionListener(new Equals());
@@ -87,11 +97,15 @@ public class FourFunctionCalculator extends JPanel {
 		public void actionPerformed(ActionEvent e)
 		{
 			calcText.setEnabled(true);
-			if(calcText.getText().equals("0"))
+			if(pushed || calcText.getText().equals("0"))
 			{
+				calcText.setText("");
+				pushed = false;
 				if(row == 100 && col == 100)
 				{
 					calcText.setText("0");
+				} else if(row == 101 && col == 101) {
+					calcText.setText(".");
 				} else {
 					calcText.setText(numButtons[row][col].printVal());
 				}
@@ -99,6 +113,8 @@ public class FourFunctionCalculator extends JPanel {
 				if(row == 100 && col == 100)
 				{
 					calcText.setText(calcText.getText() + "0");
+				} else if(row == 101 && col == 101) {
+					calcText.setText(calcText.getText() + ".");
 				} else {
 					calcText.setText(calcText.getText() + numButtons[row][col].printVal());
 				}
@@ -110,6 +126,17 @@ public class FourFunctionCalculator extends JPanel {
 	{
 		public void actionPerformed(ActionEvent e)
 		{
+			pushes++;
+			pushed = true;
+			System.out.println(pushes);
+			if(pushes > 1)
+			{
+				nums[1] = Double.parseDouble(calcText.getText());
+				calculate();
+			}
+			
+			nums[0] = Double.parseDouble(calcText.getText());
+			
 			if(e.getSource() == add)
 				op = add.getOp();
 			else if(e.getSource() == subtract)
@@ -119,9 +146,11 @@ public class FourFunctionCalculator extends JPanel {
 			else if(e.getSource() == divide)
 				op = divide.getOp();
 			
-			nums[0] = Integer.parseInt(calcText.getText());
-			calcText.setEnabled(false);
-			calcText.setText("");
+			System.out.print("changed op");
+			
+			
+			System.out.println(nums[0]);
+			//calcText.setText("");
 			
 		}
 	}
@@ -131,22 +160,12 @@ public class FourFunctionCalculator extends JPanel {
 		public void actionPerformed(ActionEvent e)
 		{
 			
-			nums[1] = Integer.parseInt(calcText.getText());
+			nums[1] = Double.parseDouble(calcText.getText());
 			
-			double answer = 0;
-			
-			if(op.equals(add.getOp()))
-				 answer = nums[0] + nums[1];
-			else if(op.equals(subtract.getOp()))
-				 answer = nums[0] - nums[1];
-			else if(op.equals(multiply.getOp()))
-				 answer = nums[0] * nums[1];
-			else if(op.equals(divide.getOp()))
-				 answer = (double)nums[0] / nums[1];
-			
-			calcText.setText("" + answer);
+			calculate();
 		}
 	}
+	
 	
 	private class Clear implements ActionListener
 	{
@@ -156,8 +175,27 @@ public class FourFunctionCalculator extends JPanel {
 			nums[1] = 0;
 			op = "";
 			calcText.setText("0");
+			pushes = 0;
 		}
 	}
+	
+	public void calculate()
+	{
+		double answer = 0;
+		
+		if(op.equals(add.getOp()))
+			 answer = nums[0] + nums[1];
+		else if(op.equals(subtract.getOp()))
+			 answer = nums[0] - nums[1];
+		else if(op.equals(multiply.getOp()))
+			 answer = nums[0] * nums[1];
+		else if(op.equals(divide.getOp()))
+			 answer = (double)nums[0] / nums[1];
+		
+		calcText.setText("" + answer);
+		System.out.println(calcText.getText());
+	}
+	
 	public static void main(String[] args)
 	{
 		if(true)
